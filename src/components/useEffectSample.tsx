c10d4fb1764504d6eef3c50a5fba2bfa8e91d1dc
Y.K <powdersugar828828@gmail.com>
2022-04-30 01:52:34 +0900

    useEffectに関する内容追記。

--- src/components/useEffectSample.tsx
+++ src/components/useEffectSample.tsx
@@ -1,23 +1,44 @@
 import React, { useEffect, useState } from 'react';
 
-const Counter2 = () => {
+const Counter2: React.FC = () => {
   const [counter, setCounter] = useState(0);
 
   const countUp = () => setCounter(counter+ 1);
-
   const countDown = () => setCounter(counter - 1);
 
+  const [counterSecond, setCounterSecond] = useState(0);
+
+  // Sample1
   // レンダリングされたタイミングで処理を実行する
   // レンダリングされるたびにconsole.log('レンダーされました。');が実行される。
   // componentDidMount、componentDidUpdate、componentWillUnmountを全指定したようなイメージ。
   useEffect(() => {
     console.log('レンダーされました。');
+
+    // 第2引数に何も渡さない状態でstateを変更すると無限ループを引き起こす。
+    //
+    // 1, コンポーネントがレンダリングされた時にstateが変更される
+    // 2, stateが変更されたことによってCounter2というコンポーネントが再度レンダリングされる
+    // 3, 再レンダリングされたコンポーネントの中でまたstateが変更される（1に戻る）
+    //
+    // というループに陥るから。
+    // setCounter(counter + 1);
+
     return () => {
       // clean up処理を記述する。
       console.log('クリーンアップします。');
     };
   });
 
+  // Sample2
+  // componentDidMount時 + componentDidUpdate時に処理を実行する
+  // 以下のコードだとstate counterの値が変化した時のみ再レンダリングが実行される
+  // counterSecondが変化した時は再レンダリングは実行されない
+  // useEffect(() => {
+  //   console.log(`counter: ${counter}`);
+  // }, [counter])
+
+  // Sample3
   // useEffectで再レンダーされた場合に実行する処理を記述する。
   // useEffect(() => {
   //   console.log('レンダーされました。');
@@ -40,6 +61,11 @@ const Counter2 = () => {
         <p>counterは{counter}です。</p>
         <button onClick={countUp}>+1</button>
         <button onClick={countDown}>-1</button>
+      </div>
+      <div>
+        <p>counterSecondは{counterSecond}です。</p>
+        <button onClick={() => setCounterSecond(counterSecond + 1)}>+1</button>
+        <button onClick={() => setCounterSecond(counterSecond - 1)}>-1</button>
       </div>
       <div className='line'></div>
     </>
